135e580dd9d63338886615f32af235a0a2b0c0a3
Stan Fortoński <contact@stanfortonski.dev>
2020-04-24 19:08:05 +0200

    Clear

--- game/js/SOSEngine/test/src/staticGroup.js
+++
@@ -1,8 +0,0 @@
-SOSEngine.Test.tests.push(function(){
-	QUnit.module("Static Group Test");
-	
-	QUnit.test("Constructor", function(assert){
-		let group = new SOSEngine.StaticObject($('#GROUPTEST'));
-		assert.strictEqual(group.layer, 50, "Layer is correct");
-	});
-});
